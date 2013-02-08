24d0d3a8380df0d38fd321a0ed5d989d4c220f03
todd haldeman <trh178@gmail.com>
2013-02-08 15:48:45 -0500

    refined get line command. fixed populate board function.

--- sudoku.hs
+++ sudoku.hs
@@ -21,22 +21,15 @@ initBoard = take 81 $ repeat $ [1..9]
 populateBoard :: Board -> String -> Board
 populateBoard [] _ = []
 populateBoard _ [] = []
-populateBoard (b:bs) (s:ss) = delete (digitToInt s) b : populateBoard bs ss 
-
+populateBoard (b:bs) (s:ss)
+  | cell > 0 = [digitToInt s] : populateBoard bs ss 
+  | otherwise = b : populateBoard bs ss
+  where cell = digitToInt s
+    
 main :: IO ()
 main = do
-  rs1 <- getLine
-  rs2 <- getLine
-  rs3 <- getLine
-  rs4 <- getLine
-  rs5 <- getLine
-  rs6 <- getLine
-  rs7 <- getLine
-  rs8 <- getLine
-  rs9 <- getLine
-
-  let board = populateBoard initBoard boardString
-        where boardString = rs1 ++ rs2 ++ rs3 ++ rs4 ++ rs5 ++ rs6 ++ rs7 ++ rs8 ++ rs9
+  lines <- sequence $ take 9 $ repeat getLine
+  let board = populateBoard initBoard (concat lines)
 
   print board
   
