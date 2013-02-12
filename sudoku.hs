b6c9a6dc964dfa267af1bb2a3abde31e3fbfdef7
todd haldeman <trh178@gmail.com>
2013-02-12 15:01:23 -0500

    doesnt build currently. type error.

--- sudoku.hs
+++ sudoku.hs
@@ -4,9 +4,7 @@ import Data.List
 
 --type Solution = [Int]
 type Possibility = [Int]
-type Row = [Possibility]
-type Col = [Possibility]
-type Box = [Possibility]
+type Region = [Possibility]
 type Board = [Possibility] 
 
 
@@ -16,46 +14,65 @@ everyf n [] = []
 everyf n as = head as : everyf n (drop n as)
 every n k = everyf n . drop (k-1)
 
+teveryf :: Int -> [a] -> [a]
+teveryf n [] = []
+teveryf n as = union (take 3 as) (teveryf n (drop n as))
+tevery n k = teveryf n . drop (k - 1)
+
 
 -- MODULE FUNCTIONS
 
-extractRow :: Board -> Int -> Row
+extractRow :: Board -> Int -> Region
 extractRow b i = take 9 $ drop (i - 1) $ b
 
-extractCol :: Board -> Int -> Col
+extractCol :: Board -> Int -> Region
 extractCol b i = every 9 i b
 
-extractBox :: Board -> Int -> Box
-extractBox = undefined
+extractBox :: Board -> Int -> Region
+extractBox b i = take 9 $ tevery 9 k b
+    where k = [1,4,7,28,31,34,55,58,61] !! (i-1)
 
-rowSweep :: Row -> Row
-rowSweep r =
-  let singles = concat $ filter (null . tail) r
-  in map (\x -> if length x > 1 then x \\ singles else x) r
+s_transpose :: Board -> Board
+s_transpose b = helper b 1
+    where helper b i
+            | i > 9 = []
+            | otherwise = union (extractCol b i) (helper b (i + 1))
 
-colSweep :: Col -> Col
-colSweep = undefined
+s_btransform :: Board -> Board
+s_btransform b = helper b 1
+    where helper b i
+             | i > 9 = []
+             | otherwise = union (extractBox b i) (helper b (i + 1))
 
-boxSweep :: Box -> Box
-boxSweep = undefined
-
-checkRows :: Board -> Board
-checkRows [] = []
-checkRows b = union (rowSweep r) (checkRows rs)
-          where r = take 9 b
-                rs = drop 9 b
+sweep :: Region -> Region
+sweep r =
+    let singles = concat $ filter (null . tail) r
+    in map (\x -> if length x > 1 then x \\ singles else x) r
 
 finished :: Board -> Bool
 finished [] = True
 finished (b:bs)
-  | length b > 1 = False
-  | otherwise = finished bs
+    | length b > 1 = False
+    | otherwise = finished bs
 
-solveBoard :: Board -> Board
-solveBoard b
-  | finished b = b
-  | otherwise = checkRows b
-    
+checkRows :: Board -> Board
+checkRows [] = []
+checkRows b = union (sweep r) (checkRows rs)
+    where r = take 9 b
+          rs = drop 9 b
+
+checkCols :: Board -> Board
+checkCols = s_transpose . checkRows . s_transpose
+
+checkBoxes :: Board -> Board
+checkBoxes = s_btransform . checkRows . s_btransform
+
+solveBoard :: [(Board -> Board)] -> (Board -> Bool) -> Board -> Board
+solveBoard (f:fs) term board
+    | term nextBoard = nextBoard
+    | otherwise = solveBoard fs term nextBoard
+    where nextBoard = f board
+
 initBoard :: Board
 initBoard = take 81 $ repeat $ [1..9]
 
@@ -63,9 +80,9 @@ populateBoard :: Board -> String -> Board
 populateBoard [] _ = []
 populateBoard _ [] = []
 populateBoard (b:bs) (s:ss)
-  | cell > 0 = [digitToInt s] : populateBoard bs ss 
-  | otherwise = b : populateBoard bs ss
-  where cell = digitToInt s
+    | cell > 0 = [digitToInt s] : populateBoard bs ss 
+    | otherwise = b : populateBoard bs ss
+    where cell = digitToInt s
 
 
 -- MAIN
@@ -73,7 +90,7 @@ main :: IO ()
 main = do
   lines <- sequence $ take 9 $ repeat getLine
   let board = populateBoard initBoard (concat lines)
-  let solution = solveBoard board
+  let solution = solveBoard (cycle [checkRows, checkCols, checkBoxes]) finished board
   
   print board
   
