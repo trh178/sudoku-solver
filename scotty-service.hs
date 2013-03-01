b768e793a58dd2d16caae9d3b682e093f5d3c38c
todd haldeman <trh178@gmail.com>
2013-03-01 15:59:18 -0500

    scotty service has basic outline in. it will respond to root requests with instructions. it will respond to blank solve request with instructions. it will no yet solve or generate anything. it just has placeholders for now. it also has no error handling yet.

--- scotty-service.hs
+++ scotty-service.hs
@@ -4,8 +4,16 @@ import Data.Monoid (mconcat)
 import Sudoku
 
 main = scotty 3000 $ do
-  get "/:word" $ do
-    beam <- param "word"
-    html $ mconcat ["<h1>scotty, ", beam, " me up!</h1>"]
 
+  -- in case someone requests root
+  get "/" $ text "Try requesting /generate or /solve/<puzzle>"
+
+  -- generate a puzzle
+  get "/generate" $ text "<generated puzzle here>"
+
+  get "/solve" $ text "Try passing a puzzle: /solve/<puzzle>"
+
+  get "/solve/:puzzle" $ do
+    p <- param "puzzle"
+    html $ mconcat ["<h1>", p, "</h1>"]
     
